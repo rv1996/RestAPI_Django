e8cce754b19dc17f4a7bc6aea43b9006e15aa85f
user.name <vermarupanshu733@gmail.com>
2016-11-06 23:58:55 +0530

    Added class based view from rest_framework of django and tested with curl request and httpie form

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,73 @@
 from django.shortcuts import render
-
+from rest_framework import status
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import Http404
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 # Create your views here.
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self,data,**kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content,**kwargs)
+
+
+# used fro the client who don't have the csrf token
+
+class SnippetList(APIView):
+    """
+    List all code snippets, or create a new snippet.
+    """
+
+    def get(self, request, format=None):
+        snippet = Snippet.objects.all()
+        serializer = SnippetSerializer(snippet,many=True)
+        return JSONResponse(serializer.data)
+
+    def post(self, request, format=None):
+        # data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SnippetDetail(APIView):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    def get_object(self,pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self,request, pk, format=None):
+        snippet = self.get_object(pk=pk)
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+
+    def put(self,request, pk, format=None):
+        snippet = self.get_object(pk=pk)
+        serializer = SnippetSerializer(snippet,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.data,status=400)
+
+    def delete(self,request, pk, format=None):
+        snippet = self.get_object(pk=pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
